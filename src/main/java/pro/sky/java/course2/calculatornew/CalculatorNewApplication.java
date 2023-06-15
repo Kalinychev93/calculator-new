6a5570c4f7e41b5553c7ab010e370222e4061442
alex_kalinychev <alex.kalinychev@yandex.ru>
2023-06-15 21:00:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/calculatornew/CalculatorNewApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculatornew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorNewApplication.class, args);
+	}
+
+}
